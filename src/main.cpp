f6141b4f5f83009114de25dbb514bfe46690fd7b
aymenbkzz <aboukezzata@hotmail.fr>
2021-11-20 23:12:49 +0100

    aymen

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,71 @@
 #include <Arduino.h>
+#include <HardwareSerial.h>
+#include "rn2xx3.h"
+
+#define RXD2 18
+#define TXD2 19
+#define RESET 21
+
+rn2xx3 lora(Serial2);
+
+void initialize_radio()
+{
+  //reset RN2xx3
+  pinMode(RESET, OUTPUT);
+  digitalWrite(RESET, LOW);
+  delay(100);
+  digitalWrite(RESET, HIGH);
+
+  delay(100); //wait for the RN2xx3's startup message
+  Serial2.flush();
+
+  /*//check communication with radio
+  String hweui = lora.hweui();
+  while(hweui.length() != 16)
+  {
+    Serial.println("Communication with RN2xx3 unsuccessful. Power cycle the board.");
+    Serial.println(hweui);
+    delay(10000);
+    hweui = lora.hweui();
+  }
+
+  //print out the HWEUI so that we can register it via ttnctl
+  Serial.println("When using OTAA, register this DevEUI: ");
+  Serial.println(hweui);
+  Serial.println("RN2xx3 firmware version:");
+  Serial.println(lora.sysver());
+
+  //configure your keys and join the network
+  Serial.println("Trying to join TTN");
+  bool join_result = false;
+
+  //ABP: initABP(String addr, String AppSKey, String NwkSKey);
+  join_result = lora.initABP("02017201", "8D7FFEF938589D95AAD928C2E2E7E48F", "AE17E567AECC8787F749A62F5541D522");
+
+  //OTAA: initOTAA(String AppEUI, String AppKey);
+  //join_result = lora.initOTAA("70B3D57ED00001A6", "A23C96EE13804963F8C2BD6285448198");
+
+  while(!join_result)
+  {
+    Serial.println("Unable to join. Are your keys correct, and do you have TTN coverage?");
+    delay(60000); //delay a minute before retry
+    join_result = lora.init();
+  }
+  Serial.println("Successfully joined TTN");*/
+
+}
+
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial2.begin(57600, SERIAL_8N1, RXD2, TXD2);
+  
+  initialize_radio();
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println(lora.getRx());
 }
