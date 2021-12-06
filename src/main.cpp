be79b7e370f321594138bc9fbdf65c344cdd0ac1
MeslouhArselan96 <meslouhma@hotmail.com>
2021-12-06 15:51:15 +0100

    piste bleue final

--- src/main.cpp
+++ src/main.cpp
@@ -1,69 +1,56 @@
 #include <Arduino.h>
 #include "rn2xx3.h"
 
-#define RST 21
-#define RX 18
-#define TX 19
-#define INTERRUPT_PIN 12
+#define RST 21 //Pin de reset du module LORA
+#define RX 18  //Pin RX du module LORA
+#define TX 19  //Pin TX du module LORA
+#define INTERRUPT_PIN 12 //Pin d'interruption du capteur de GAZ
+#define SIG_grove 2  //Signal d'entree du capteur grove (industriel)
+#define Buzz_pin 17  //Pin de sortie du buzzer
+#define R_Poly_CMD 16 //Pin de la sortie de commande de la resistance de chauffe (Polysilicium)
+#define ADC_SENS 33 // Pin d'entree du signal du capteur de gaz (AIME)
+#define ADC_R_Alu 27 //Pin d'entree de la resistance d'ALUMINIUM (capteur de temperature)
 
-#define SIG 2
-#define Buzz_pin 17
-#define R_Poly_CMD 16
-#define ADC_SENS 33
-#define ADC_R_Alu 27
-
-
-
-
- // RX, TX !! labels on relay board is swapped !!
-rn2xx3 myLora(Serial2);
-byte payload[2];
-
-float R0=0.45;
-
-float sensor_volt;
-float RS_gas; // Get value of RS in a GAS
+int a; //
+int b; // R_aluminium = a * T(Kelvin) + b 
+rn2xx3 myLora(Serial2); //Constructeur de la classe rn2xx3
+byte payload[4]; //Payload a envoyer sur TTN
+float R0=0.45; //Resistance du module grove en environement ambiant
+float sensor_grove_volt; //conversion en volt de l'entree du signal du capteur grove
+float sensor_aime_volt; //conversion en volt de l'entree du signal du capteur AIME
+float RS_gas; //recoit la valeur de RS dans le gaz
 float ratio; // Get ratio RS_GAS/RS_air
-
- int duty_cyle;
-  int t_1 = 0;
-  int t_2 = 0;
-  int delta_t=t_2 - t_1;
-  double Kp=0.8, Ki=10, Kd=0.1;
-  float e_t = 0;
-  float e_t_1 = 0;
-
-uint32_t groove_data;
-uint32_t aime_data;
-
-int R1;
-int R2;
-int R3;
-int R5;
-int Vcc;
-int R6;
-
- // the number of the LED pin
-const uint32_t ledPin = 16;  // 16 corresponds to GPIO16
-
-// setting PWM properties
+int duty_cyle; //duty cycle du signal PWM pour la commande de la resistance de chauffe
+int t_1 = 0; //timestamp 1
+int t_2 = 0; //timestamp 2
+int delta_t=t_2 - t_1; // delta time
+double Kp, Ki, Kd; //Ponderations du PID (à definir en observant la reponse du systeme)
+float e_t = 0; //mesure de l'erreur a t
+float e_t_1 = 0; //mesure de l'erreur a t-1
+uint32_t grove_data; //donnée capteur grove
+uint32_t aime_data;  //donnée capteur AIME
+int R1; //
+int R2; //
+int R3; //
+int R5;  //Resistances du circuit de conditionnement selon la meme numerotation que dans KICAD !
+int Vcc; // A definir en fonction des grandeurs electriques voulues
+int R6; //
+const uint32_t PWM_OUT = 16;  //Pin de sortie de la commande PWM de la resistance de chauffe
+// setup des propriétés PWM
 const uint32_t freq = 20000;
 const uint32_t ledChannel = 0;
 const uint32_t resolution = 8;
 
-void initialize_radio()
+void initialize_radio() //Initialization du module LORA
 {
-  //RST RN2xx3
+  //Reset du module
   pinMode(RST, OUTPUT);
   digitalWrite(RST, LOW);
   delay(100);
   digitalWrite(RST, HIGH);
-
-  delay(100); //wait for the RN2xx3's startup message
+  delay(100); //Attente du message de start du module LORA
   Serial2.flush();
-
-
-  //check communication with radio
+  //check de la communication avec la radio
   String hweui = myLora.hweui();
   while(hweui.length() != 16)
   {
@@ -72,105 +59,69 @@ void initialize_radio()
     delay(10000);
     hweui = myLora.hweui();
   }
-
-  //print out the HWEUI so that we can register it via ttnctl
+  //Recuperation de HWEUI pour la configurer sur TTN
   Serial.println("When using OTAA, register this DevEUI: ");
   Serial.println(hweui);
   Serial.println("RN2xx3 firmware version:");
   Serial.println(myLora.sysver());
-
-
-  //configure your keys and join the network
+  //message de debug de type "en attente de connexion"
   Serial.println("Trying to join TTN");
   bool join_result = false;
-
-  //ABP: initABP(String addr, String AppSKey, String NwkSKey);
-  //join_result = myLora.initABP("260BC0E0", "DDA46D8053E8E61D270FC829FB8E3041", "91077773C17E66C8E99DAA4FE049DD7C");
-
   //OTAA: initOTAA(String AppEUI, String AppKey);
   join_result = myLora.initOTAA("0000000000000000", "44E53245F1200CDEEC2525661C76BA38");
-
   while(!join_result)
   {
     Serial.println("Unable to join. Are your keys correct, and do you have TTN coverage?");
-    delay(60000); //delay a minute before retry
+    delay(60000); //delay d'une minute avant retry 
     join_result = myLora.init();
   }
   Serial.println("Successfully joined TTN");
-  
-}
+  }
 
 void manage_interrupt(){
-
 }
 
-
-void setup() {
-    // Note the format for setting a serial port is as follows: Serial2.begin(baud-rate, protocol, RX pin, TX pin);
-  Serial.begin(9600);
-  // Open serial communications and wait for port to open:
-  config_PWM();
-  Serial2.begin(57600, SERIAL_8N1, RX, TX);
-  initialize_radio();
-  Serial2.print("sys get hwei\r\n");
-  Serial.println(Serial2.readStringUntil('\n'));
-
-}
-
-uint32_t read_groove_sensor (){
-  int sensor_groove_Value = analogRead(SIG);
-  sensor_volt=(float)sensor_groove_Value/4096*5.0;
-  RS_gas = (5.0-sensor_volt)/sensor_volt; // omit *RL
+uint32_t read_grove_sensor () //Recuperation de la donnée du capteur grove
+{
+  int sensor_groove_Value = analogRead(SIG_grove); 
+  sensor_grove_volt=(float)sensor_groove_Value/4096*5.0; //conversion en tention
+  RS_gas = (5.0-sensor_grove_volt)/sensor_grove_volt; // TAUX VARIATION RESISTANCE SELON GAZ
   ratio = (RS_gas/R0);  // ratio = RS/R0
-  /*-----------------------------------------------------------------------*/
-  delay(1000);
   uint32_t data=ratio*100;
-
   return data;
-
 }
 
-uint32_t read_aime_sensor (){
+uint32_t read_aime_sensor () //Recuperation de la donnée du capteur AIME
+{
   int sensor_aime_Value = analogRead(ADC_SENS);
-  sensor_volt=(float)sensor_aime_Value/4096*5.0;
-  /*float v_sens = sensor_aime_Value/(10^7);
-  float i_sens= v_sens */
-
-  float R_capteur=(R1*(R2+R3)*Vcc/(R2*sensor_volt)) -R1-R5;
- 
+  sensor_aime_volt=(float)sensor_aime_Value/4096*5.0;
+  float R_capteur=(R1*(R2+R3)*Vcc/(R2*sensor_aime_volt)) -R1-R5; //formule justifiée en TD
   return R_capteur;
-
 }
 
-uint32_t read_r_alu (){
+uint32_t read_r_alu () //Recuperation de la donnée du capteur de temparateur (R ALU)
+{
   int ADC_R_ALU = analogRead(ADC_R_Alu);
   float ADC_R_ALU_volt=(float)ADC_R_Alu/4096*5.0;
-  /*float v_sens = sensor_aime_Value/(10^7);
-  float i_sens= v_sens */
-
   float R_Alu=ADC_R_ALU_volt*R6/(3.3-ADC_R_ALU_volt);
-
- 
   return R_Alu;
-
-}
-void config_PWM(){
-
-
-// configure PWM functionalitites
-ledcSetup(ledChannel, freq, resolution);
-
-// attach the channel to the GPIO to be controlled
-ledcAttachPin(ledPin, ledChannel);
-
 }
 
-float temperature (){
+void config_PWM() // configure des fonctionnalités PWM
+{
+  ledcSetup(ledChannel, freq, resolution);
+  ledcAttachPin(PWM_OUT, ledChannel); //Liaison de la chaine du Timer avec la pin utilisée
+}
+
+float temperature () // mesure de la temperature du capteur en fonction de R aluminium
+{
   float T = a*(read_r_alu()) + b;
+  T = T - 273; //conversion en Celcius
   return T;
 }
-int manage_PID(){
- 
+
+int manage_PID() //Correcteur PID pour maintenir la temprature a 200 degrés celcius
+{
   int t_2 = micros();
   e_t = 200 - temperature();
   float e_P = Kp * e_t;
@@ -179,32 +130,32 @@ int manage_PID(){
   t_2 = t_2;
   e_t_1 = e_t;
   uint8_t C = e_P + e_D + e_I;
-  if(C > 255) C = 255;
+  if(C > 255) C = 255; //Pour borner la correction entre 0 et 255
  
-  return C;
+  return C; //Duty cycle de la PWM de commande
 }
 
-void Cmd_PWM_Poly(){
-int duty_cycle=manage_PID();
-ledcWrite(ledChannel,duty_cyle)
-
+void Cmd_PWM_Poly() //Commande de la resistance de chauffe en PWM
+{
+  int duty_cycle=manage_PID();
+  ledcWrite(ledChannel,duty_cyle);
 }
 
-
+void setup() {
+  Serial.begin(9600); //initialisation moniteur serie (ESP - PC)
+  config_PWM(); //configuration de la PWM
+  Serial2.begin(57600, SERIAL_8N1, RX, TX); //initialisation moniteur serie 2 (LORA - ESP)
+  initialize_radio(); //initialiation de la radio
+  Serial2.print("sys get hwei\r\n"); // recuperation du HWEUI
+  Serial.println(Serial2.readStringUntil('\n'));
+  
+}
 void loop() {
-
-  groove_data=read_groove_sensor();
+  grove_data=read_grove_sensor();
   aime_data=read_aime_sensor();
-
-
-  Serial.println("TXing");
-  Serial.println(ratio);
-  delay(1000);
-
-  payload[0]=highByte(gas_data);  
-  payload[1]=lowByte(gas_data);
-
-  myLora.txBytes(payload,2); //one byte, blocking function
- 
-
+  payload[0] = highByte(grove_data);
+  payload[1] = lowByte(grove_data);
+  payload[2]=highByte(aime_data);  
+  payload[1]=lowByte(aime_data);
+  myLora.txBytes(payload,4); 
 }
